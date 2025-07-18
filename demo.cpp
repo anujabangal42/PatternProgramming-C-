93fed13e4ed44dd71c4b390b7f79e752af3d5945
Anuja Bangal <anujabangal42@gmail.com>
2025-07-18 15:53:56 +0530

    Add files via upload

--- demo.cpp
+++ demo.cpp
@@ -1,26 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
+  
+
+
+void print( int n)
+{
+   for(int i=0;i<n;i++)
+   {
+     for(int j=0;j<=i;j++){
+      
+      cout<<"*";
+
+     }
+     cout<<endl;
+   }
+
+}
 
 int main()
 
 {
-   for(int i=0;i<4;i++)
-   {
-    
-      for(int j=0;j<4;j++)
-      {
-        cout<<"*";
-      }
-     cout<<endl;
-   }
-   
+    int n;
+    cin>>n;
+    print(n);
 }
-
-
-
-
-output:
-****
-****
-****
-****
